c57fee0d25004966b9e0e3d72719057c66c88755
AdelFouda01 <168671815+AdelFouda01@users.noreply.github.com>
2024-05-01 21:50:31 +0300

    update UART

--- MCAL/UART.h
+++ MCAL/UART.h
@@ -1,1 +1,16 @@
+#ifndef UART
+#define UART
+
+
+void UART1_Init();
+void UART_Write (char data);
+char UART_GetChar  (void);
+void UART_recieve_String( char* string , int size);
+void UART_Send_String( char* string);
+
+#endif
+
+
+
+
 
