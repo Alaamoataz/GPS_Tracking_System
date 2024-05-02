0cf320cce5d7f6ae2fa487de4d3b48055ab188b6
Mohamed-Kh20 <mohamedkh1080@gmail.com>
2024-05-02 09:16:58 +0300

    Update GPS.h

--- HAL/GPS.h
+++ HAL/GPS.h
@@ -1,5 +1,10 @@
+#ifndef GPS_H
+#define GPS_H
+
 #define EARTH_RADIUS 6371000
 #define pi 3.142857142857143
 float GPSgetDistance(float currentLong, float currentLat, float destLong, float destLat);
 float ToDegree(float angle);
-////float ToRad(float angle);
+float ToRad(float angle);
+
+#endif
