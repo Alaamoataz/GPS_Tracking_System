64012fc30a7c19b4c810d42458d0aa4d263371fb
AbdalRahman-megahed <71913604+AbdalRahman-megahed@users.noreply.github.com>
2024-05-16 23:00:17 +0300

    Update Uart.h

--- MCAL/UART.h
+++ MCAL/UART.h
@@ -1,16 +1,18 @@
-#ifndef UART
-#define UART
+#ifndef UART_H
+#define UART_H
+#include <stdint.h>
 
+void uart2_init();
+void uart0_init();
+void Uart5_init(void);
+void uart5_send_byte(char c);
+void Uart5_output_string(char* pt);
+void uart2_send_byte(char c);
+char uart2_read_byte(void);
+void uart0_send_byte(char data);
+void Uart0_output_string(char* pt);
+void UART0SendFloat(float num);
+void UART0SendString(const char *str);
 
-void UART1_Init();
-void UART_Write (char data);
-char UART_GetChar  (void);
-void UART_recieve_String( char* string , int size);
-void UART_Send_String( char* string);
 
 #endif
-
-
-
-
-
