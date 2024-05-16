8b5d4f6a3cba7dc4f0474e85bd47cc32b671a581
Km-abdelsalam <168653745+Km-abdelsalam@users.noreply.github.com>
2024-05-16 22:46:17 +0300

    GPIO C

--- MCAL/GPIO.c
+++ MCAL/GPIO.c
@@ -1,52 +1,15 @@
-#include "../../Lib/Bit_utilies.h"
-#include "../../Lib/TM4C123.h"
+#include "../LIB/tm4c123gh6pm.h"
 #include "GPIO.h"
-//============================================================================  
 
-void GPIO_init_PORTF()
-{
-  SYSCTL_RCGCGPIO_R |= 0x20;   // Enable clock for Port F
-  while((SYSCTL_PRGPIO_R & 0x20) == 0);   // Wait for Port F to be ready
-	GPIO_PORTF_LOCK_R  = GPIO_LOCK_KEY;   							
-	GPIO_PORTF_CR_R    = GPIO_ENABLE_8_BIT;						
-	GPIO_PORTF_AFSEL_R = 0x00;  //Disable AFSEL
-	GPIO_PORTF_AMSEL_R = 0x00;	//Disable AMSEL											
-	GPIO_PORTF_PCTL_R  = GPIO_PCTL_VALUE; 						 
-	GPIO_PORTF_DEN_R   = 0xFF;	//Digital Enable All Pins
-	GPIO_PORTF_PUR_R = 0x11;	//Enable pull up to Pin0-4
-}
-
-//============================================================================
-
-void GPIO_init_PORTB()
-{
-  SYSCTL_RCGCGPIO_R |= 0x02;   // Enable clock for Port B
-  while((SYSCTL_PRGPIO_R & 0x02) == 0);   // Wait for Port B to be ready
-	GPIO_PORTF_LOCK_R  = GPIO_LOCK_KEY; 							
-	GPIO_PORTF_CR_R    = GPIO_ENABLE_8_BIT;						
-	GPIO_PORTF_AFSEL_R = 0x00;	//Disable AFSEL
-	GPIO_PORTF_AMSEL_R = 0x00;	//Disable AMSEL											
-	GPIO_PORTF_PCTL_R  = GPIO_PCTL_VALUE; 						 
-	GPIO_PORTF_DEN_R   = 0xFF;		//Digital Enable All Pins
-}
-   
-//============================================================================  
-
-void Set_direction_PORTF(unsigned char Pin_no , unsigned char Pin_dir)
-{
-	switch (Pin_dir)
-	{
-	//Make Pin Digital output
-	case OUTPUT:
-	SET_BIT(GPIO_PORTF_DIR_R , Pin_no);
-	break;
-
-	//Make Pin Digital input
-	case INPUT :
-	CLR_BIT(GPIO_PORTF_DIR_R , Pin_no);
-	break;
-	
-	default:
-	break;
-	}
+void portF_init(void){
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;// Enable port F clock
+	while((SYSCTL_PRGPIO_R & SYSCTL_PRGPIO_R5) == 0){};
+	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;		// Unlock PortF PF0
+	GPIO_PORTF_CR_R = 0x1F;					// Allow changes to PF1-3
+	GPIO_PORTF_DIR_R = 0x0E;				// PF3,PF2,PF1 output
+	GPIO_PORTF_AFSEL_R = 0x00;				// No alternate function
+	GPIO_PORTF_PCTL_R = 0x00000000;			// GPIO clear bit PCTL
+	GPIO_PORTF_DEN_R = 0x1F;				// Enable digital pins PF1-PF3
+	GPIO_PORTF_AMSEL_R = 0x00;				// Disable analog function
+	GPIO_PORTF_PUR_R = 0x11;        // enable pull-up on?PF0?and?PF4
 }
