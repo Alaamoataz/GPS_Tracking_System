e9ae1d4a012d66d765284e285082cd7899a4f012
Baselm78 <120133642+Baselm78@users.noreply.github.com>
2024-05-16 21:28:10 +0200

    .

--- LIB/Bit_utilies.h
+++ LIB/Bit_utilies.h
@@ -5,11 +5,11 @@
 #define GET_REG(reg)        (reg&0xff)
 
 #define SET_REG(reg,val)        reg |= val
-#define SET_BIT(reg,val)    reg |= (1<<val)
+#define SET_BIT(reg,val)        reg |= (1<<val)
 #define TOG_BIT(reg,val)		reg ^= (1<<val)
 
 #define CLR_REG(reg,val)        reg &=~val
-#define CLR_BIT(reg,val)    reg &=~(1<<val)
+#define CLR_BIT(reg,val)        reg &=~(1<<val)
 
 #endif
 
