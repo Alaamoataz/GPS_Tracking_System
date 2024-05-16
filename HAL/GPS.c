f5c66de394100331aebdeadb5bacebee45da5b2b
Alaamoataz <alaamoatz94@gmail.com>
2024-05-16 22:37:18 +0300

    final_actual_gps

--- HAL/GPS.c
+++ HAL/GPS.c
@@ -1,21 +1,22 @@
-#include "gps.h"
-
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../MCAL/UART.h"
+#include "GPS.h"
+#include "LED.h"
 
-#include "led.h"
-#include "uart.h"
-
-char GPS_logname[] = "$GPRMC";
-char GPS[80];
+char GPS_logname[]="$GPRMC";
+char GPS[800];
 char GPS_formated[12][20];
-char* token;
+
+char * token;
 float currentLat;
 float currentLong;
+float Speed; 
 
-// extract $GPRMC message content
-void GPS_read(void) {
+//extract $GPRMC message content
+
+void GPS_read(void){
     char i = 0;
     char counter = 0;
 
@@ -32,28 +33,61 @@ void GPS_read(void) {
         GPS[counter] = uart2_read_byte();
         counter++;
     } while (GPS[counter - 1] != '*');
+	}
+
+
+
+// extract Latitude  and Longitude of the $GPRMC message 
+void GPS_format(void){
+	char noofStrings = 0;
+	RGB(BLUE_LED);	
+	token = strtok (GPS ,",");
+
+	do{
+	strcpy (GPS_formated [noofStrings],token);
+	token = strtok (NULL ,",");
+	noofStrings++;
+	}while (token!=NULL);
+	
+		Speed = atof(GPS_formated[6]);
+		if(strcmp(GPS_formated[1], "A")==0) {
+
+				if (strcmp(GPS_formated [3], "N")==0)
+				currentLat = atof (GPS_formated [2]);
+				else
+				currentLat = -(atof (GPS_formated [2]));
+
+				if (strcmp(GPS_formated [5], "E")==0)
+				currentLong = atof (GPS_formated [4]);
+				else
+				currentLong = -(atof (GPS_formated [4]));
+
+				}
+
 }
 
-// extract Latitude  and Longitude of the $GPRMC message
-void GPS_format(void) {
-    char noofStrings = 0;
-    token = strtok(GPS, ",");
-
-    do {
-        strcpy(GPS_formated[noofStrings], token);
-        token = strtok(NULL, ",");
-        noofStrings++;
-    } while (token != NULL);
-
-    if (strcmp(GPS_formated[1], "A") == 0) {
-        if (strcmp(GPS_formated[3], "N") == 0)
-            currentLat = atof(GPS_formated[2]);
-        else
-            currentLat = -(atof(GPS_formated[2]));
-
-        if (strcmp(GPS_formated[5], "E") == 0)
-            currentLong = atof(GPS_formated[4]);
-        else
-            currentLong = -(atof(GPS_formated[4]));
-    }
+double to_degree(float raw_degree){  //gps output to degrees
+	int dd = (int) (raw_degree / 100);
+    double mm = raw_degree - (dd * 100);
+    double degree = dd + (mm / 60);
+		
+    return degree;
 }
+
+
+double to_radians(double degrees) {   // degree to rad
+    return degrees * M_PI / 180.0;
+}
+
+double distance(double lat1, double lon1, double lat2, double lon2) {  //calc distance between 2 coordinates in M
+    double dlat = to_radians(lat2 - lat1);
+    double dlon = to_radians(lon2 - lon1);
+    double a = pow(sin(dlat / 2), 2) + cos(to_radians(lat1)) * cos(to_radians(lat2)) * pow(sin(dlon / 2), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return RADIUS * c;
+}
+
+double approximate( double a, float d) {
+		return (int)(a/d +0.5)*d;
+}
+
