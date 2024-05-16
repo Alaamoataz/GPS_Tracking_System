226a33c61373ed93023a628dcdad69608b8bf608
FatmaKh24 <168669109+FatmaKh24@users.noreply.github.com>
2024-05-16 22:58:07 +0300

    update

--- HAL/LED.c
+++ HAL/LED.c
@@ -1,79 +1,18 @@
-#include "../../Lib/Bit_utilies.h"
-#include "../../Lib/TM4C123.h"
-#include "../../MCAL/GPIO/GPIO.h"
+#include "../LIB/tm4c123gh6pm.h"
 #include "LED.h"
-//led initalization
-void Led_Switch_Init(void)
-{
-    GPIO_init_PORTF();
-    Set_direction_PORTF(BLUE_LED ,OUTPUT);   
-    Set_direction_PORTF(GREEN_LED ,OUTPUT);   
-    Set_direction_PORTF(RED_LED ,OUTPUT);   
-    Set_direction_PORTF(switch1 ,INPUT);   
-    Set_direction_PORTF(switch2 ,INPUT);   
-    
-}
-//switch on led
-void LED_On(unsigned char Led_Colour)
-{
-    
-    switch(Led_Colour)
-    {
-        case BLUE_LED:
-        SET_BIT(GPIO_PORTF_DATA_R, BLUE_LED);
-        break;
-        case GREEN_LED:
-        SET_BIT(GPIO_PORTF_DATA_R, GREEN_LED);
-        break;
-        case RED_LED:
-        SET_BIT(GPIO_PORTF_DATA_R, RED_LED);
-        break;
-    }
-}
-//switch off led
-void LED_Off(unsigned char Led_Colour)
-{
-    switch(Led_Colour)
-    {
-        case BLUE_LED:
-        CLR_BIT(GPIO_PORTF_DATA_R, BLUE_LED);
-        break;
 
-        case GREEN_LED:
-        CLR_BIT(GPIO_PORTF_DATA_R, GREEN_LED);
-        break;
 
-        case RED_LED:
-        CLR_BIT(GPIO_PORTF_DATA_R, RED_LED);
-        break;
-    }
+void RGB_set(char mask){
+    mask &= 0X0E;
+    GPIO_PORTF_DATA_R=mask;
 }
 
-//change led situation
-void LED_TOG(unsigned char Led_Colour)
-{
-    switch(Led_Colour)
-    {
-        case BLUE_LED:
-        TOG_BIT(GPIO_PORTF_DATA_R , BLUE_LED);
-        break;
-
-        case GREEN_LED:
-        TOG_BIT(GPIO_PORTF_DATA_R , GREEN_LED);
-        break;
-
-        case RED_LED:
-        TOG_BIT(GPIO_PORTF_DATA_R , RED_LED);
-        break;
-    }
+void RGB_clear(char mask){
+    mask &= 0X0E;
+    GPIO_PORTF_DATA_R &= ~mask;
 }
 
-
-unsigned char Switch_input(unsigned char switch_no)
-{
-    switch(switch_no)
-    {
-        case switch1: return GET_BIT(GPIO_PORTF_DATA_R,switch1); break;
-        case switch2: return GET_BIT(GPIO_PORTF_DATA_R,switch2); break;
-    }   
+void RGB(char colour){
+	GPIO_PORTF_DATA_R &= ~RGB_LED;			// Stop all active LEDs
+	GPIO_PORTF_DATA_R |= colour;			// Turn on this led
 }
